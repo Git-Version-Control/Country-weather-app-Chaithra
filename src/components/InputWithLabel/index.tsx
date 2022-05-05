8f56ec6fc2d062e18142107d6eebfb1bfda4a9d0
Chaithra <102819533+Git-Version-Control@users.noreply.github.com>
2022-05-05 16:27:09 +0530

    Add files via upload

--- src/components/InputWithLabel/index.tsx
+++ src/components/InputWithLabel/index.tsx
@@ -1,1 +1,33 @@
 
+import { style } from "@mui/system";
+import { ChangeEvent, FC } from "react";
+import styles from "./index.module.css";
+
+type InputWithLabelProps = {
+  //labelName: string;
+  id: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+  placeholder: string;
+};
+
+const InputWithLabel: FC<InputWithLabelProps> = ({
+ // labelName,
+  id,
+  value,
+  placeholder,
+  onChange,
+}) => (
+  <div className={styles.container}>
+      
+    <input
+      className={styles.input}
+      id={id}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+    />
+  </div>
+);
+
+export default InputWithLabel;
